6ea3692f8b29ad985a47db6eab62ffbc3b3b4259
sandeshgiri98 <132637865+sandeshgiri98@users.noreply.github.com>
2024-02-20 08:00:44 +0545

    Add files via upload
    Viewing data of the required user

---
+++ Database4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DatabaseConnection4
+{
+    internal class DisplayById
+    {
+        public void ShowDetailsById()
+        {
+            string cs = "Data Source=NEXUS\\SQLEXPRESS;Initial Catalog=dotnet_swsc;Integrated Security=true;";
+            try
+            {
+                using (SqlConnection sc = new SqlConnection(cs))
+                {
+                    sc.Open();
+                    Console.WriteLine("Enter the ID of a person whose record you want to display:");
+                    string id = Console.ReadLine();
+
+                    // Select Query...
+                    string displayQuery = "SELECT * FROM tbl_reg WHERE id = @id";
+                    SqlCommand cmd = new SqlCommand(displayQuery, sc);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader row = cmd.ExecuteReader();
+                    while (row.Read())
+                    {
+                        Console.WriteLine("ID: " + row["id"]);
+                        Console.WriteLine("Username: " + row["username"]);
+                        Console.WriteLine("Password: " + row["password"]);
+                        Console.WriteLine("Repassword: " + row["repassword"]);
+                        Console.WriteLine("Gender: " + row["gender"]);
+                        Console.WriteLine("Course: " + row["course"]);
+                        Console.WriteLine("Country: " + row["country"]);
+                        Console.WriteLine("------------------------------------");
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            
+        }
+    }
+}
