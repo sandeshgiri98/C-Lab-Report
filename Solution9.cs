dd3831e572f9054703fb1a885144fc1f95a2635e
sandeshgiri98 <132637865+sandeshgiri98@users.noreply.github.com>
2024-02-23 20:51:19 +0545

    Add files via upload
    solution 9

---
+++ Solution9.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Practical9
+{
+    class ParentClass
+    {
+        protected int baseNumber;
+
+        public ParentClass(int number)
+        {
+            baseNumber = number;
+        }
+
+        public void ShowBaseNumber()
+        {
+            Console.WriteLine("Base Number: " + baseNumber);
+        }
+    }
+    class ChildClass : ParentClass
+    {
+        private int derivedNumber;
+
+        public ChildClass(int baseNumber, int derivedNumber) : base(baseNumber)
+        {
+            this.derivedNumber = derivedNumber;
+        }
+
+        public void ShowDerivedNumber()
+        {
+            Console.WriteLine("Derived Number: " + derivedNumber);
+        }
+
+        public void ShowBothNumbers()
+        {
+            Console.WriteLine("Base Number: " + baseNumber); 
+            Console.WriteLine("Derived Number: " + derivedNumber);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+}
