012bc7faee450d75fb809c6fd932f18ca63ef400
sandeshgiri98 <132637865+sandeshgiri98@users.noreply.github.com>
2024-02-23 20:50:59 +0545

    Add files via upload
    solution 8

---
+++ Solution8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Practical8
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Animal is sleeping.");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Dog is barking.");
+        }
+    }
+
+    class BabyDog : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("BabyDog is weeping.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+       
+    }
+
+}
