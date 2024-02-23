030a130870e8d46cd7d785b8c89c677308f8ca2a
sandeshgiri98 <132637865+sandeshgiri98@users.noreply.github.com>
2024-02-23 20:52:07 +0545

    Add files via upload
    solution 10

---
+++ Solution10.cs
@@ -0,0 +1,35 @@
+﻿//Write a program to demonstrate method overriding (dynamic polymorphism)? 
+using System;
+namespace Practical10
+{
+    class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a shape");
+        }
+    }
+    class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a circle");
+        }
+    }
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a rectangle");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
+
