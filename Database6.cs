a1a1ffed098ffe308e3eed66b75c8c4200a646c3
sandeshgiri98 <132637865+sandeshgiri98@users.noreply.github.com>
2024-02-23 20:50:18 +0545

    Add files via upload
    solution 6

---
+++ Database6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+namespace DatabaseConnection6
+{
+    internal class DeleteById
+    {
+        public void Delete()
+        {
+            try
+            {
+                string cs = "Data Source =NEXUS\\SQLEXPRESS;Initial Catalog = dotnet_swsc; Integrated Security = true; "; 
+                SqlConnection sc = new SqlConnection(cs);
+
+                if (sc.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("server connected");
+                }
+                sc.Open();
+                Console.WriteLine("enter id to delete record..");
+                string delid = Console.ReadLine();
+
+                string deleteQuery = "delete from tbl_reg where id =@id"; SqlCommand cmd = new SqlCommand(deleteQuery, sc); cmd.Parameters.AddWithValue("@id", delid); int res = cmd.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    Console.WriteLine("Deleted Successfully");
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+    class program
+    {
+        public static void Main(String[] args)
+        {
+        }
+    }
+}
